82e7078fb565eb43e7a847deddba216a7a8e63bf
Andras Bogoly <ebogoly@gmail.com>
2017-03-14 15:32:58 +0100

    Partially working build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: {
+        main: './src/index.js',
+        vendor: 'jquery'
+    },
+    output: {
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                use: 'css-loader'
+            })
+        }],
+    loaders: [
+        {
+            test: /\.hbs$/,
+            loader: 'handlebars-loader'
+        }
+    ]
+  },
+  plugins: [
+        new ExtractTextPlugin('[name].[chunkhash].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.hbs',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/de/index.hbs',
+            filename: 'de/index.html'
+        })
+  ]
 };
