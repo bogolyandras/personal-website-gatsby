fa0a90d91197e8faf0abbf6c736116110d753b1b
Andras Bogoly <ebogoly@gmail.com>
2017-03-14 16:07:15 +0100

    Build now works

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -38,6 +39,22 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: 'src/de/index.hbs',
             filename: 'de/index.html'
-        })
+        }),
+        new CopyWebpackPlugin([
+            { from: 'static/403.html', to: '.' },
+            { from: 'static/404.html', to: '.' },
+            { from: 'static/error.html', to: '.' },
+            { from: 'static/favicon.ico', to: '.' },
+            { from: 'static/manifest.json', to: '.' },
+            { from: 'static/robots.txt', to: '.' },
+            { from: 'static/icon/android-icon.png', to: 'icon/.' },
+            { from: 'static/icon/apple-icon.png', to: 'icon/.' },
+            { from: 'static/icon/favicon.png', to: 'icon/.' },
+            { from: 'static/icon/flickr.png', to: 'icon/.' },
+            { from: 'static/icon/instagram.png', to: 'icon/.' },
+            { from: 'static/icon/linkedin.png', to: 'icon/.' },
+            { from: 'static/icon/ms-icon.png', to: 'icon/.' },
+            { from: 'static/WEB-INF/web.xml', to: 'WEB-INF/.' }
+        ])
   ]
 };
