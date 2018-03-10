b695ebda49b9332aaab1e554a48e7a536fb84b85
Andras Bogoly <andras.bogoly.91@gmail.com>
2018-03-10 21:54:38 +0100

    Page layout fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <div className={'album-skeleton'}>
-            <div className={'container'}>
-    <h1>Not found 😿</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className={'container'}>
+      <h1>Not found 😿</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
   </div>
 )
