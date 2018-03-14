bf03d032835edc5cc3c5566df4221627efd7ad9b
Andras Bogoly <ebogoly@gmail.com>
2018-03-14 16:55:25 +0100

    list update

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,31 +1,21 @@
+import ReactDOM from 'react-dom'
 import React from 'react'
 import Link from 'gatsby-link'
 
-const websites = {
-  "asdasd": {
-    "asdasd": "asdasdasd",
-    "dasd": "asdasdasdas"
-  },
-  "dasdas": {
-    "fdfsdf": "fsdfsdfsdf",
-    "fsdfsdf": "sdfsdfsdfsdfsdf"
-  },
-  "dasdas": {
-    "fdfsdf": "fsdfsdfsdf",
-    "fsdfsdf": "sdfsdfsdfsdfsdf",
-    "fsdfsdsadf": "sdfsdfsdfsdfsdf",
-    "fsddasduhfsdf": "sdfsdfsdfsdfsdf"
-  }
-}
+const websites = ["asdasd", "fgsduiobhsdo", "afasfasf"];
 
-const SecondPage = () => (
+const listItems = websites.map((website) =>
+  <li key={website.toString()}>{website}</li>
+)
+
+const List = () => (
+
   <div className={'album-skeleton'}>
     <div className={'container'}>
-      <h1>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
+      <ul>{listItems}</ul>
     </div>
   </div>
-);
+  
+)
 
-export default SecondPage
+export default List
