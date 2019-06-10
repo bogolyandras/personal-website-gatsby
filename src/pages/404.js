0d5e121f0957fb9a966de26be3435c63a999f1cf
Andras Bogoly <andras.bogoly.91@gmail.com>
2019-06-10 18:40:21 +0200

    404 fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,12 @@
 import React from "react"
-import Layout from "../components/layout";
+import Layout from "../components/layout"
+import Container from "../components/container"
 
 export default () => (
   <Layout>
-    <div>
-        <h1>Not found <span role="img" aria-label="Sad cat">😿</span></h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <Container>
+      <h1>Not found <span role="img" aria-label="Sad cat">😿</span></h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Container>
   </Layout>
 )
