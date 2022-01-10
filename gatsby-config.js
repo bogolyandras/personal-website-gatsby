c67f552d51d776e89d6ee85f177f63f0ad01f73c
András Bögöly <andras.bogoly.91@gmail.com>
2022-01-10 23:11:55 +0100

    Update to gatsby 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `András Bögöly's Personal homepage`,
-    description: `Personal website`,
-    author: `@bogolyandras`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -18,13 +20,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `András Bögöly's personal website`,
-        short_name: `András Bögöly`,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
         start_url: `/`,
-        background_color: `#325c80`,
-        theme_color: `#325c80`,
-        display: `browser`, // https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,14 @@
-import React from "react"
-import Layout from "../components/layout"
-import Container from "../components/container"
-import SEO from "../components/seo"
+import * as React from "react"
 
-export default () => (
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const NotFoundPage = () => (
   <Layout>
-    <SEO title="Not found" />
-    <Container>
-      <h1>Not found <span role="img" aria-label="Sad cat">😿</span></h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Container>
+    <Seo title="404: Not found" />
+    <h1>404: Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
+
+export default NotFoundPage
