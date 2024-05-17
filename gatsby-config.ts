2c594e046b8b51941f8c40c44a4fed50e6ad93c8
András Bögöly <andras.bogoly.91@gmail.com>
2024-05-17 15:17:49 +0000

    Update to gatsby 5.13

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,38 +10,48 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      name: `András Bögöly's personal website`,
-        short_name: `András Bögöly`,
-        start_url: `/`,
-        background_color: `#325c80`,
-        theme_color: `#325c80`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: `András Bögöly's personal website`,
+          short_name: `András Bögöly`,
+          start_url: `/`,
+          background_color: `#325c80`,
+          theme_color: `#325c80`,
+          display: `minimal-ui`,
+          icon: `src/images/icon.png`
+      }
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: `pages`,
-      path: `./src/pages/`
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
     },
-    __key: "pages"
-  }, {
-    resolve: "gatsby-source-filesystem",
-    options: {
-      name: `blog`,
-      path: `./blog/`
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `pages`,
+        path: `./src/pages/`
+      },
+      __key: "pages"
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `./blog/`
+      }
     }
-  }]
+  ]
 };
 
 export default config;
