0945c0d08ec6c84a247067de283aaa7d4aa9c5e3
András Bögöly <andras.bogoly.91@gmail.com>
2024-05-23 22:03:32 +0200

    Update to gatsby 5.13

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import * as React from "react"
 import type { HeadFC } from "gatsby"
 import Layout from '../components/layout'
 import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <Layout path="index">
       <h1>Welcome to my world!</h1>
         <p>Here you will find some interesting stuff! Although there are not too many things, you can take a look around.</p>
+        <StaticImage src="../images/Firefly a city of cats 99761.jpg" alt="A city of cats" width="1000" />
         <p>Follow me on</p>
         <ul>
             <li><a href="https://www.linkedin.com/in/bogolyandras/">LinkedIn</a></li>
